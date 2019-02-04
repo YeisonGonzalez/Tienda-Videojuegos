982f7a36e309f6d7296746de424de38fafcf074c
jsneisav <43756353+jsneisav@users.noreply.github.com>
2019-02-03 19:08:32 -0500

    Add files via upload

--- Tienda de videojuegos/src/businesslogic/Main.java
+++ Tienda de videojuegos/src/businesslogic/Main.java
@@ -32,6 +32,7 @@ public class Main {
 		String[] nin= {"nintendo"};
 		String[] xbox= {"xbox"};
 		String[] pc = {"pc"};
+		String[] play = {"playstation"};
 		String[] par = {"proteger","sobrevivir","derrotar"};
 		String[] map = {"selva","ruinas","desierto","ciudad"};
 		String[] sourc= {"piedra"};
@@ -40,20 +41,52 @@ public class Main {
 		String[] nada = {"nada"};
 		String[] car= {"sprint", "circuito","contrarreloj","drift", "carro vs avion"};
 		String[] mkx= {"alien","mortal kombat","depredador","jason","Leatherface"};
+		String[] mvc= {"marvel universe","capcom"};
 		String[] dif= {"facil","normal","dificil","heroica","legendaria"};
+		
  	Plataformas deadcells =new Plataformas (las4 ,false,true,true,360,9.5,3,11,500000, "lo que sea que signifique ludonarratividad",25,"isometrica 3d", 100, 3,obstaculos, enemigos) ;
 	
+ 	Plataformas Crashbandicoot =new Plataformas (las3 ,false,true,true,360,9,3,25,500000, "lo que sea que signifique ludonarratividad",10,"isometrica 3d", 100, 1,obstaculos, enemigos) ;
+ 	
 	Bullethell cuphead = new Bullethell(xpc ,false,true,true,0,9.5,7,11,500000, "lo que sea que signifique ludonarratividad",25,"2d", 100, "retro", obstaculos, enemigos);
+	
+	Bullethell NierAutomata = new Bullethell(las3 ,false,true,true,360,9.5,12,19,500000, "lo que sea que signifique ludonarratividad",25,"3ra persona", 100, "futurista", obstaculos, enemigos);
+	
+	estrategia commandandconquer = new estrategia(las4, true,true,false,360,9.0,12,20,100000,"lo que sea ",par, map,sourc);
+	
+	estrategia civilizationII = new estrategia(las4, true,true,false,360,9.0,12,20,100000,"lo que sea ",par, map,sourc);
+	
+	Aventura unravel = new Aventura(xpc,false,true,true,360,9.5,7,11,500000, "lo que sea que signifique ludonarratividad","naaaada","suecia", enemigos);
+	
+	Aventura neveralone = new Aventura(las4,false,true,false,220,9.4,7,15,30000,"xd","par","artico",nada);
+	
+	MundoAbierto dishonored = new MundoAbierto(las4,true,true,false,195,9.0,15,"ubisoft","unreal engine 4",nada,25,500,500);
+	
+	MundoAbierto gtaV = new MundoAbierto(las3, true,true,false,160,9.9,18,"rockstar games","no se",nada,60,400,1000);
 	
 	Simulacion fifa19 = new Simulacion(las4,true,true,true,360,8.0,3,"EA spors cheneke", "frosbite",nada,"futbolito",5000,nada,nada,nada,2018);
 	
+	Simulacion NBA2k19 = new Simulacion(las4,true,true,true,360,8.0,3,"2k", "frosbite",nada,"basketball",5000,nada,nada,nada,2018);
+	
 	EstiloLibre supermariostricker = new EstiloLibre(las3,true,true,true,360,9.0,3,"nextlevel", "no se",nada,"futbolito",16,map,nada,nada,nada);
+	
+	EstiloLibre supermariotennis = new EstiloLibre(las3,true,true,true,360,9.0,3,"nextlevel", "no se",nada,"tennis",16,map,nada,nada,nada);
+	
+	Carreras forzamotorsport7 = new Carreras(xpc,true,true,false,170,9.0,3,"turn 10 studios", "forza tech", nada, nada,false,car);
+	
+	Carreras mariokart = new Carreras(nin,true,true,true,170,9.0,3,"nintendo", "no se", nada, nada,false,car);
 	
 	OneVsOne mortalkombatx = new OneVsOne(las4,true,true,true,360,9.0,18," NetherRealm Studios","unreal engine",mkx,20,10,nada,50,nada,"ahjsdfma");
 	
+	OneVsOne marvelvscapcom = new OneVsOne(las4,true,true,true,360,9.0,18," NetherRealm Studios","unreal engine 4",mvc,20,10,nada,50,nada,"ahjsdfma");
+	
 	PvE halo = new PvE(xpc,true,true,true,150,9.0,15,"343 y bungie","no se",nada,20,10,nada,300,52,arm,30,dif,nada);
 	
-	PvPRoyale fortnite = new PvPRoyale(las4,true,true,false,150,9.0,7,"epic games","no se",nada,20,10,nada,100,52,arm,25,"no se como se llama",nada);
+	PvE carlosdutymw3 = new PvE(las3,true,true,true,150,9.0,15,"activision","unreal engine",nada,20,10,nada,300,52,arm,30,dif,nada);
+	
+	PvPRoyale fortnite = new PvPRoyale(las4,true,true,false,150,9.0,7,"epic games","no se",nada,20,10,nada,100,52,arm,25,"solo me se señorio de la sal",nada);
+	
+	PvPRoyale pubg = new PvPRoyale(las4,true,true,false,150,9.0,7,"PUBG Corporation","unreal engine 4",nada,20,10,nada,100,52,arm,25,"no se como se llama",nada);
 	
 	System.out.println("Bienvenido, seleccione el tipo de juego quieres comprar:");
 	System.out.println("1-Indie"+"\n"+"2-AAA");
